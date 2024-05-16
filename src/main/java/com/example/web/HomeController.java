094aed161a38ba73e32dbdc42a818ab9b57e0485
sujit <sujittech06@gmail.com>
2024-05-16 16:21:12 +0530

    code changes

--- src/main/java/com/example/web/HomeController.java
+++ src/main/java/com/example/web/HomeController.java
@@ -1,10 +1,10 @@
 package com.example.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/home")
 public class HomeController {
 @GetMapping("/get")
